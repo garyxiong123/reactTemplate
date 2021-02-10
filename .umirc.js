e057a337e699d2ba38ad41b723414f6eddf2a63b
熊成威 <xiongchengwei@yofish.com>
2021-02-10 21:43:39 +0800

    fix

---
+++ .umirc.js
@@ -0,0 +1,36 @@
+// ref: https://umijs.org/config/
+export default {
+  treeShaking: true,
+  routes: [
+    {
+      path: '/',
+      component: '../layouts/index',
+      routes: [
+        {
+          path: '/products',
+          component: './products',
+        },
+        {
+          path: '/',
+          component: '../pages/index',
+        },
+      ],
+    },
+  ],
+  plugins: [
+    // ref: https://umijs.org/plugin/umi-plugin-react.html
+    [
+      'umi-plugin-react',
+      {
+        antd: false,
+        dva: false,
+        dynamicImport: false,
+        title: 'app',
+        dll: false,
+        routes: {
+          exclude: [/components\//],
+        },
+      },
+    ],
+  ],
+};
